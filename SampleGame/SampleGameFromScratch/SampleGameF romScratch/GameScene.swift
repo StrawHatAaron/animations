78dc73f715a49af4158f4f7edad9aaf5b630c904
Aubrey Miller <aubreymiller@aubreys-mbp.attlocal.net>
2018-06-18 21:55:19 -0700

    dkdlfsajfl

---
+++ SampleGame/SampleGameFromScratch/SampleGameF romScratch/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  SampleGameF romScratch
+//
+//  Created by Aaron Miller on 6/17/18.
+//  Copyright © 2018 Aaron Miller. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+ 
+}
